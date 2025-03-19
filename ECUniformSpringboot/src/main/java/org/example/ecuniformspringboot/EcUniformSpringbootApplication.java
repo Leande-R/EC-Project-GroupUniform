fc6b0985ba99d22d641ddf5e62fd8d4070759ac7
Leande-R <131276205+Leande-R@users.noreply.github.com>
2025-03-19 13:50:37 +0100

    SpringbootProjectCreated

---
+++ ECUniformSpringboot/src/main/java/org/example/ecuniformspringboot/EcUniformSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecuniformspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcUniformSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcUniformSpringbootApplication.class, args);
+	}
+
+}
